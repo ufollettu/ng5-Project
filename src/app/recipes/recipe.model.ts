4ca1c138698501b7ccf324abcf6a0d9828893cf0
Pasquale Merolle <pasquale.merolle@gmail.com>
2018-03-28 21:26:05 +0200

    add ingredients to recipe

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -5,10 +5,12 @@ export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, description: string, imagePath: string) {
+  constructor(name: string, description: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,36 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 @Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [  //typescript way to use a model as an array
-    new Recipe('Pollu cusutu nculu', 'lu pollu', 'https://1.bp.blogspot.com/-ZhUOHZu4qt8/WKiWcAYhkMI/AAAAAAAAB6g/jQFtRRDqacIF4hDeZaasNenJusnPXvfnACLcB/s1600/lu-pollu-cosutu-nculu-checco-zalone.jpg'),
-    new Recipe('Nduja', 'Sample recipe description', 'http://www.buongustaidicalabria.it/1086-large_dm/nduja-calabrese-di-spilinga-in-budello-artigianale-l-originale-.jpg'),
-    new Recipe('Polenta e Osei', 'Sample recipe description', 'http://www.buonalombardia.regione.lombardia.it/wps/wcm/connect/b711e12d-ea65-495c-8cff-8ad56f66d313/polenta+e+osei.jpg?MOD=AJPERES&CACHEID=b711e12d-ea65-495c-8cff-8ad56f66d313')
+    new Recipe(
+      'Pollu cusutu nculu',
+      'lu pollu',
+      'https://1.bp.blogspot.com/-ZhUOHZu4qt8/WKiWcAYhkMI/AAAAAAAAB6g/jQFtRRDqacIF4hDeZaasNenJusnPXvfnACLcB/s1600/lu-pollu-cosutu-nculu-checco-zalone.jpg',
+      [
+        new Ingredient('pollo', 1),
+        new Ingredient('peperoncino', 10)
+      ]),
+    new Recipe(
+      'Nduja',
+      'Sample recipe description',
+      'http://www.buongustaidicalabria.it/1086-large_dm/nduja-calabrese-di-spilinga-in-budello-artigianale-l-originale-.jpg',
+      [
+        new Ingredient('porco', 1),
+        new Ingredient('peperoncino', 50)
+      ]),
+    new Recipe(
+      'Polenta e Osei',
+      'Sample recipe description',
+      'http://www.buonalombardia.regione.lombardia.it/wps/wcm/connect/b711e12d-ea65-495c-8cff-8ad56f66d313/polenta+e+osei.jpg?MOD=AJPERES&CACHEID=b711e12d-ea65-495c-8cff-8ad56f66d313',
+      [
+        new Ingredient('mais', 100),
+        new Ingredient('uccellini', 5)
+      ])
   ];
 
   getRecipes() {
