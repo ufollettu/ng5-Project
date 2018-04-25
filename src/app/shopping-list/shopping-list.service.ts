d6fb015cb5c4366a37b7d9eb8cd77faa07451596
Pasquale Merolle <pasquale.merolle@gmail.com>
2018-04-26 00:15:37 +0200

    remote adding

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Ingredient} from "../shared/ingredient.model";
+import {Subject} from "rxjs/Subject";
 
 @Injectable()
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('farina', 500),
@@ -19,7 +20,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice()); // return a copy of the array
+    this.ingredientsChanged.next(this.ingredients.slice()); // return a copy of the array
   }
 
   addIngredients(ingredients: Ingredient[]) {
@@ -45,6 +46,6 @@ export class ShoppingListService {
     });
 
     // this.ingredients.push(...ingredients); // spread operator
-    this.ingredientsChanged.emit(this.ingredients.slice()); // return a copy of the array
+    this.ingredientsChanged.next(this.ingredients.slice()); // return a copy of the array
   }
 }
